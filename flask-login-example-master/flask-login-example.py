3216b489f7633eae8aedc99955c2af4df3b86036
JulenZZZ <jzulaikake16dw@ikzubirimanteo.com>
2017-05-10 18:33:12 +0200

    Update flask-login-example.py

--- flask-login-example-master/flask-login-example.py
+++ flask-login-example-master/flask-login-example.py
@@ -1,86 +1,27 @@
-from flask import Flask, Response, redirect, url_for, request, session, abort
-from flask.ext.login import LoginManager, UserMixin, \
-                                login_required, login_user, logout_user 
+from flask import Flask
+from flask import render_template
+from flask import request, url_for, redirect
 
-app = Flask(__name__)
+app=Flask(__name__)
+app.debug=True
 
-# config
-app.config.update(
-    DEBUG = True,
-    SECRET_KEY = 'secret_xxx'
-)
-
-# flask-login
-login_manager = LoginManager()
-login_manager.init_app(app)
-login_manager.login_view = "login"
-
-
-# silly user model
-class User(UserMixin):
-
-    def __init__(self, id):
-        self.id = id
-        self.name = "user" + str(id)
-        self.password = self.name + "_secret"
-        
-    def __repr__(self):
-        return "%d/%s/%s" % (self.id, self.name, self.password)
-
-
-# create some users with ids 1 to 20       
-users = [User(id) for id in range(1, 21)]
-
-
-# some protected url
 @app.route('/')
-@login_required
+def index():
+    return "Hola Mundo"
+
+@app.route('/home')
 def home():
-    return Response("Hello World!")
-
- 
-# somewhere to login
-@app.route("/login", methods=["GET", "POST"])
+    return render_template("home.html")
+@app.route('/login', methods=['GET','POST'])
 def login():
+    error = None
     if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']        
-        if password == username + "_secret":
-            id = username.split('user')[1]
-            user = User(id)
-            login_user(user)
-            return redirect(request.args.get("next"))
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error= "Login Failed. El usuario o la contraseña es incorrecta."
         else:
-            return abort(401)
-    else:
-        return Response('''
-        <form action="" method="post">
-            <p><input type=text name=username>
-            <p><input type=password name=password>
-            <p><input type=submit value=Login>
-        </form>
-        ''')
+            return redirect(url_for('home'))
+        return render_template('login.html', error=error)
 
-
-# somewhere to logout
-@app.route("/logout")
-@login_required
-def logout():
-    logout_user()
-    return Response('<p>Logged out</p>')
-
-
-# handle login failed
-@app.errorhandler(401)
-def page_not_found(e):
-    return Response('<p>Login failed</p>')
-    
-    
-# callback to reload the user object        
-@login_manager.user_loader
-def load_user(userid):
-    return User(userid)
-    
 
 if __name__ == "__main__":
     app.run()
