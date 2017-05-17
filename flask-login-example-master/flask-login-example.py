ee3a86dfe9685fdd92161d3ffcd9280da2e6208b
JulenZZZ <jzulaikake16dw@ikzubirimanteo.com>
2017-05-17 12:07:54 +0200

    Update flask-login-example.py

--- flask-login-example-master/flask-login-example.py
+++ flask-login-example-master/flask-login-example.py
@@ -2,16 +2,20 @@ from flask import Flask
 from flask import render_template
 from flask import request, url_for, redirect
 
+
+
 app=Flask(__name__)
 app.debug=True
+app.config['SECRET_KEY']='secret_key'
 
 @app.route('/')
 def index():
-    return "Hola Mundo"
+    return render_template("index.html")
 
 @app.route('/home')
 def home():
     return render_template("home.html")
+
 @app.route('/login', methods=['GET','POST'])
 def login():
     error = None
@@ -22,6 +26,22 @@ def login():
             return redirect(url_for('home'))
     return render_template('login.html', error=error)
 
+@app.route("/insert_post", methods=['GET'])
+def route_insert_post():
+    # GET
+    post_name = request.args.get('post_name', '')
+    author_name = request.args.get('author_name', '')
+    descripcion = request.args.get('descripcion','')
+
+    insert_post(post_name ,author_name ,descripcion)
+    
+    return redirect('/select_all_students')
+
+    
+@app.route('/logout')
+def logout():
+    flash="Acabas de cerrar sesión!"
+    return render_template('login.html', flash=flash)
 
 if __name__ == "__main__":
     app.run()
