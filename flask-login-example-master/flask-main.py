ebecdb16409f42c6c78cd6440c661ed75ab5eee4
JulenZZZ <jzulaikake16dw@ikzubirimanteo.com>
2017-05-24 13:17:45 +0200

    Update flask-main.py

--- flask-login-example-master/flask-main.py
+++ flask-login-example-master/flask-main.py
@@ -50,7 +50,7 @@ def logout():
     flash="Acabas de cerrar sesión!"
     return render_template('login.html', flash=flash)
 #funcion para borrar post en desarrollo
-@app.route('/delete_post', methods=['GET'])
+@app.route('/delete_post', methods=['POST'])
 def delete():
     name_borrar = request.args.get('delete', '')
     post_name = request.args.get('post_name', '')
